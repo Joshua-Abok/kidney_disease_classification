1485525731f7cad57866f0670b7ad9826c6abd04
Joshua Abok <nyajuaya.j.a@gmail.com>
2023-10-24 20:07:39 +0300

    data ingestion | data pipeline

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,7 @@
+# path here is constant so easily read 
+#         config.yaml & params.yaml
+
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
