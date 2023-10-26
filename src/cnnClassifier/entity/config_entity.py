7096b5f164378f24be3c2901a3b61a0ec99c8c3b
Joshua Abok <nyajuaya.j.a@gmail.com>
2023-10-26 05:49:15 +0300

    modularize prepare base model

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -11,3 +11,15 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path                       # everything written in config.yaml file
 
+
+# update entity from 02_prepare_base_model
+@dataclass(frozen=True)    # don't want any changes that's why frozen=True
+class PrepareBaseModelConfig: 
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool 
+    params_weights: str
+    params_classes: int
